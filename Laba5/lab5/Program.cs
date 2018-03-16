7cb5a9b82b7017e86674eed79566e4e76eab9bd1
alina1307 <alina.beskempirova@mail.ru>
2018-03-16 12:04:11 +0600

    Laba5

---
+++ Laba5/lab5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Max searcher = new Max();
+            searcher.SearchTheMaximum();
+        }
+    }
+}
