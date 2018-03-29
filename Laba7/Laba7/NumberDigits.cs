f32dc5f6376e643f080ede8bf7d663e6a96fbee0
alina1307 <alina.beskempirova@mail.ru>
2018-03-29 22:19:53 +0600

    Laba7

---
+++ Laba7/Laba7/NumberDigits.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7
+{
+    class NumberDigits
+    {
+        public List<int> Input ()
+        {
+            Random r = new Random();
+            List<int> list = Enumerable.Range(0, 15).Select(n => r.Next(1, 10)).ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+            return list;
+        }
+
+        public int[] CountingNumberDigits(List<int> list)
+        {
+            int[] counter = new int[10];
+            int kol = list.Count();
+            Console.Write("Количество цифр в массиве = " + kol + "\n");
+            for (int i = 0; i < list.Count; i++)
+            {
+                counter[list[i]]++;
+            }
+            return counter;
+        }
+
+        public void Output (int[] counter)
+        {
+            for (int i = 0; i < counter.Length; i++)
+            {
+                Console.Write(" Количество " + i + " = " + counter[i] + "\n");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Laba7/Laba7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberDigits searcher = new NumberDigits();
+            List<int> list = searcher.Input();
+            int[] mas = searcher.CountingNumberDigits(list);
+            searcher.Output(mas);
+        }
+    }
+}
